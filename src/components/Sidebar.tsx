7fadd8b5d64695734582a4bc31252fe493780a33
eddieciou <a589658gur@gmail.com>
2023-01-30 17:04:22 +0800

    edit sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,9 @@
 import React, { Dispatch, SetStateAction } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+
+import logo from '../assets/eddieLogo.png';
 import { TUser } from '../types';
 
 interface ISidebar {
@@ -6,8 +11,70 @@ interface ISidebar {
   closeToggle?: Dispatch<SetStateAction<boolean>>;
 }
 
+const isActiveStyle =
+  'flex items-center px-5 gap3 font-extrabold border-r-4 border-black transition-all duration-200 ease-in-out capitalize';
+const isNotActiveStyle =
+  'flex items-center px-5 gap3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+
+const categories = [
+  { name: 'Animals' },
+  { name: 'Wallpapers' },
+  { name: 'Photography' },
+  { name: 'Gaming' },
+  { name: 'Coding' },
+  { name: 'Other' },
+];
+
 const Sidebar = ({ user, closeToggle }: ISidebar) => {
-  return <div>Sidebar</div>;
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
+      <div className='flex flex-col'>
+        <Link
+          to='/'
+          className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt='logo' className='w-full' />
+        </Link>
+        <div className='flex flex-col gap-5'>
+          <NavLink
+            to='/'
+            className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            home
+          </NavLink>
+          <h3 className='mt-2 px-5 text-base 2x:text-xl'>Discover categories</h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              key={category.name}
+              to={`/category/${category.name}`}
+              className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+              onClick={handleCloseSidebar}
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className='flex my-5 mb-3 p-2 items-center bg-white rounded-lg shadow-lg mx-3'
+          onClick={handleCloseSidebar}
+        >
+          <img src={user.image} alt='user-profile' className='w-10 h-10 rounded-full' />
+          <p>{user.userName}</p>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
