97b8314e149a5eb8f315b37358fbf1fd986a91b8
eddieciou <a589658gur@gmail.com>
2023-02-03 10:08:48 +0800

    implement Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,9 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+import { MasonryLayout, Spinner } from './index';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import { TPin } from '../types';
 
 interface ISearch {
   searchTerm: string;
@@ -6,7 +11,34 @@ interface ISearch {
 }
 
 const Search = ({ searchTerm, setSearchTerm }: ISearch) => {
-  return <div>Search</div>;
+  const [pins, setPins] = useState<Array<TPin>>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading && <Spinner message='Searching for pins..' />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins.length === 0 && searchTerm !== '' && !loading && (
+        <div className='mt-10 text-center text-xl'>No Pins Founds!</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
