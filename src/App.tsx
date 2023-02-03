52b0bac4efe9b13ee328fab0134302624bdc86e0
eddieciou <a589658gur@gmail.com>
2023-02-03 11:10:41 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 import Practice from './container/Practice';
+import { fetchUser } from './utils/fetchUser';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate('/login');
+  }, []);
+
   return (
     <Routes>
       <Route path='login' element={<Login />} />
